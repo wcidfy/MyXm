c1ff83f85cbe2c118433f05a232522a7105254c2
lxx <lxx@lixiangxiangdemacbook-pro.local>
2016-01-19 17:40:42 +0800

    Initial Commit

---
+++ MyXm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyXm
+//
+//  Created by lxx on 16/1/19.
+//  Copyright © 2016年 lxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyXm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyXm
+//
+//  Created by lxx on 16/1/19.
+//  Copyright © 2016年 lxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
